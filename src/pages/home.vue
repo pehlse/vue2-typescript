7a7cf7a9a6e4d828d36238d62aa6e7837fdd4ed3
Pedro henrique <pedro97henrique@gmail.com>
2020-09-16 22:56:57 -0300

    first commit

---
+++ src/pages/home.vue
@@ -0,0 +1,15 @@
+<template>
+  <p>{{ message }}</p>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import Component from "vue-class-component";
+
+@Component
+export default class Home extends Vue {
+  message = "teste";
+}
+</script>
+
+<style scoped></style>
